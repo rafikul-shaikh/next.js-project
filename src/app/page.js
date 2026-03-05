176e5694ff4281d36ad90cf069dbc24baf6c1463
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-03-05 10:59:15 +0530

    project is in progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import ProductCart from "@/components/ProductCart";
 
 export default function Home() {
    return (
     <>
       <Hero />
+      <ProductCart/>
     </>
   );
 }
---
+++ src/components/specification.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const specification = () => {
+  return <div>specification</div>;
+};
+
+export default specification;
