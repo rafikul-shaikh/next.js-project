3eab4c7aa7f3b3d77d4742854405d893938502a3
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-03-06 16:05:25 +0530

    bg-video and modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,34 @@
+// "use client";
+
+// export default function Hero() {
+//   return (
+//     <section className="relative h-screen w-full overflow-hidden">
+//       {/* Background Video */}
+//       <video
+//         autoPlay
+//         muted
+//         loop
+//         playsInline
+//         className="absolute top-0 left-0 w-full h-full  object-cover scale-80"
+//       >
+//         <source src="/highest.mp4" type="video/mp4" />
+//         {/* <source
+//           src="https://www.w3schools.com/html/mov_bbb.mp4"
+//           type="video/mp4"
+//         /> */}
+//       </video>
+
+//       {/* Content */}
+//       <div className="relative z-10 flex items-center justify-center h-full text-center px-6">
+//         <h1 className="text-white text-4xl md:text-6xl lg:text-7xl font-light leading-tight max-w-5xl">
+//           100% Curbside Recyclable, sealable, printable ultra high barrier
+//           paper, designed to replace flexible plastic packaging.
+//         </h1>
+//       </div>
+//     </section>
+//   );
+// }
+
 "use client";
 
 export default function Hero() {
@@ -9,18 +40,17 @@ export default function Hero() {
         muted
         loop
         playsInline
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="absolute inset-0 w-full h-full object-cover animate-videoZoom"
       >
-        <source src="/background-vdo.mp4" type="video/mp4" />
+        <source src="/highest.mp4" type="video/mp4" />
       </video>
 
-      {/* <div className="absolute inset-0">
-        <div className="absolute inset-0 bg-black/40"></div>
-      </div> */}
+      {/* Dark Overlay */}
+      <div className="absolute inset-0 bg-black/40"></div>
 
       {/* Content */}
-      <div className="relative z-10 flex items-center justify-center h-full text-center px-6">
-        <h1 className="text-white text-4xl md:text-6xl lg:text-7xl font-light leading-tight max-w-5xl">
+      <div className="relative z-10 flex items-center justify-center h-full text-center px-10">
+        <h1 className="text-white text-4xl md:text-6xl lg:text-7xl font-bold ">
           100% Curbside Recyclable, sealable, printable ultra high barrier
           paper, designed to replace flexible plastic packaging.
         </h1>
