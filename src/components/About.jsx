50f67b768e9e35ad8391f5a5cac360e186d54328
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-03-16 17:07:22 +0530

    modified files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,14 +10,14 @@ export default function About() {
 
       <div className=" max-w-full mx-auto grid grid-cols-1 md:grid-cols-12 items-start">
         {/* LEFT BADGE */}
-        <div className="md:col-span-2">
+        <div className="md:col-span-2 mb-6 md:mb-0">
           <span className="text-[12px] border border-gray-400 px-3 py-1 rounded-md text-gray-700">
             Proven Leaders in ALD
           </span>
         </div>
 
         {/* TEXT CONTENT */}
-        <div className="md:col-span-4 px-4 md:px-10 space-y-8 text-base md:text-lg leading-6 text-black">
+        <div className="md:col-span-4 md:px-10 space-y-8 text-base md:text-lg leading-6 text-black">
           <p>
             Nfinite is a spin-out from the Functional Nanomaterials Lab at the
             University of Waterloo (Canada), led by Dr. Kevin Musselman, a
@@ -34,7 +34,7 @@ export default function About() {
         </div>
 
         {/* IMAGE */}
-        <div className="Md:col-span-6 flex justify-end ml-0 md:ml-10">
+        <div className="Md:col-span-6 flex justify-end ml-0 mt-6 md:ml-10">
           <div className="relative w-full max-w-[500px] h-[350px] md:h-[600px] rounded-lg overflow-hidden">
             <img src="/Lab.avif" alt="Lab" className="object-cover" />
           </div>
